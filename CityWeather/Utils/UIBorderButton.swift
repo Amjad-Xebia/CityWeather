835cc05e736fe3aa0515dba076f306513bf993c8
Amjad Khan <amjad.khan@xebia.com>
2020-01-06 21:05:21 +0530

    customise button for border.

---
+++ CityWeather/Utils/UIBorderButton.swift
@@ -0,0 +1,26 @@
+//
+//  UIView+Layer.swift
+//  CityWeather
+//
+//  Created by Amjad Khan on 06/01/20.
+//  Copyright © 2020 Xebia. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class UIBorderButton: UIButton {
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+}
