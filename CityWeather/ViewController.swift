023348f29b92363d3bdef6fde7a9cbc96947a70b
Amjad Khan <amjad.khan@xebia.com>
2020-01-06 14:46:36 +0530

    Initial Commit

---
+++ CityWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityWeather
+//
+//  Created by Amjad Khan on 06/01/20.
+//  Copyright © 2020 Xebia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
